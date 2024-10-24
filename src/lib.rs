ac902fac05f8c8f890abf3394eb36d3f0e1a784d
omurovec <owenmurovec@gmail.com>
2024-10-24 08:51:25 -0700

    make signature module public

--- src/lib.rs
+++ src/lib.rs
@@ -19,3 +19,31 @@ pub use info::{info_client::*, *};
 pub use market_maker::{MarketMaker, MarketMakerInput, MarketMakerRestingOrder};
 pub use meta::{AssetMeta, Meta};
 pub use ws::*;
+
+// make signature module public
+pub mod pub_signature {
+    use ethers::{
+        signers::LocalWallet,
+        types::{Signature, H256},
+    };
+
+    use crate::prelude::Result;
+
+    // Public wrapper around the internal function
+    pub fn create_l1_signature(
+        wallet: &LocalWallet,
+        connection_id: H256,
+        is_mainnet: bool,
+    ) -> Result<Signature> {
+        // Call the internal function
+        super::signature::sign_l1_action(wallet, connection_id, is_mainnet)
+    }
+
+    // If you need the generic sign_typed_data function
+    pub fn create_typed_signature<T: ethers::types::transaction::eip712::Eip712>(
+        payload: &T,
+        wallet: &LocalWallet,
+    ) -> Result<Signature> {
+        super::signature::sign_typed_data(payload, wallet)
+    }
+}
